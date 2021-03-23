cec869997875fa13ca5c0d87536f5e77a078dc63
ian-1 <ian.walton@portmoor.com>
2021-03-23 20:06:51 +0000

    add input_students method

--- directory.rb
+++ directory.rb
@@ -1,17 +1,17 @@
 school = "Villains Academy"
-students = [ 
-  { name: "Dr. Hannibal Lecter", cohort: :november},
-  { name: "Darth Vader", cohort: :november},
-  { name: "Nurse Ratched", cohort: :november},
-  { name: "Michael Corleone", cohort: :november},
-  { name: "Alex DeLarge", cohort: :november},
-  { name: "The Wicked Witch of the West", cohort: :november},
-  { name: "Terminator", cohort: :november},
-  { name: "Freddy Krueger", cohort: :november},
-  { name: "The Joker", cohort: :november},
-  { name: "Joffrey Baratheon", cohort: :november},
-  { name: "Norman Bates", cohort: :november}
-]
+
+def input_students
+  students = []
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  loop do
+    name = gets.chomp
+    break if name == ""
+    students.push({name: name, cohort: :november})
+    puts  "Now we have #{students.count} students"
+  end
+  students
+end
 
 def print_header(school)
   puts "The Students of #{school}"
@@ -28,6 +28,7 @@ def print_no_of_summary(students)
   puts "Overall, we have #{students.length} great students"
 end
 
+students = input_students()
 print_header(school)
 print_list(students)
 print_no_of_summary(students)
