2ca458f6ae6dad8eeac8ebf8fa807b31f3cae566
ian-1 <ian.walton@portmoor.com>
2021-03-24 09:49:22 +0000

    add method to print list where student name starts with specific letter

--- directory.rb
+++ directory.rb
@@ -24,11 +24,21 @@ def print_list(students)
   end
 end
 
+def print_list_letter_start(students, letter)
+  i = 0
+  students.each do |student|
+    if student[:name][0] == letter
+      puts "#{i + 1}) #{student[:name]} (#{student[:cohort].capitalize} cohort)"
+      i += 1
+    end
+  end
+end
+
 def print_no_of_summary(students)
   puts "Overall, we have #{students.length} great students"
 end
 
 students = input_students()
 print_header(school)
-print_list(students)
+print_list_letter_start(students, "l")
 print_no_of_summary(students)
